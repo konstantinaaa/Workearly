232845cf74d4a361620fe3836c56bcfd3ec31aa1
konstantinaaa <113425012+konstantinaaa@users.noreply.github.com>
2023-09-28 12:25:40 +0300

    Update Simple_Symbols.py

--- Simple_Symbols.py
+++ Simple_Symbols.py
@@ -5,7 +5,7 @@ is an acceptable sequence by either returning the
 string true or false. The str parameter will be composed
 of + and = symbols with several characters between them
 (ie. ++d+===+c++==a) and for the string to be true each letter
-must be surrounded by a + symbol. So the string to the left
+must be surrounded by a +, = symbol. So the string to the left
 would be false. The string will not be empty and will have
 at least one letter.
 Examples
