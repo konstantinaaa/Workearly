309d344ffb47f42b2882fda4b5f74da6c2bb8f40
konstantinaaa <113425012+konstantinaaa@users.noreply.github.com>
2023-09-28 12:34:39 +0300

    Update Simple_Symbols.py

--- Simple_Symbols.py
+++ Simple_Symbols.py
@@ -5,7 +5,7 @@ is an acceptable sequence by either returning the
 string true or false. The str parameter will be composed
 of + and = symbols with several characters between them
 (ie. ++d+===+c++==a) and for the string to be true each letter
-must be surrounded by a +, = symbol. So the string to the left
+must be surrounded by a + symbol. So the string to the left
 would be false. The string will not be empty and will have
 at least one letter.
 Examples
@@ -14,12 +14,17 @@ Output: true
 Input: "f++d+"
 Output: false
 """
-import re
 def SimpleSymbols(strParam):
+    # Check if the first or last character is a letter
     if strParam[0].isalpha() or strParam[-1].isalpha():
         return False
-    else:
-        if re.match("[=+a-z]", strParam):
-            return True
 
-print(SimpleSymbols(input()))
+    for i in range(1, len(strParam) - 1):
+        # Check if the current character is a letter
+        if strParam[i].isalpha():
+            # Check if the characters to the left and right are not '+'
+            if strParam[i - 1] != '+' or strParam[i + 1] != '+':
+                return False
+
+    return True
+
